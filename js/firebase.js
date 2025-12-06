74fa23c8eccaa2955bb183d13b0801bd373b8518
jkkitchen <jkitchen1@byupathway.edu>
2025-12-06 18:13:44 -0500

    moved script

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,11 @@
-//This is the code given by the Firebase Website in order to import it:
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase core and Auth
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-// Your Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyC9NvKS9sWKWYFi-I4mv75I7wlrObb-mjE",
     authDomain: "swim-cut-check.firebaseapp.com",
@@ -17,8 +18,30 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Initialize Auth
-export const auth = getAuth(app);
 
-export { app };
+// //This is the code given by the Firebase Website in order to import it:
+
+// // Firebase core and Auth
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// // Your Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC9NvKS9sWKWYFi-I4mv75I7wlrObb-mjE",
+//     authDomain: "swim-cut-check.firebaseapp.com",
+//     projectId: "swim-cut-check",
+//     storageBucket: "swim-cut-check.firebasestorage.app",
+//     messagingSenderId: "120625942614",
+//     appId: "1:120625942614:web:e0932a7699ff882d586bf7",
+//     measurementId: "G-S1S5MTQEC9"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Initialize Auth
+// export const auth = getAuth(app);
+
+// export { app };
