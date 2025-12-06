4a3aec919f6d7b6c3c24b6fce1decc01c3801837
jkkitchen <jkitchen1@byupathway.edu>
2025-12-06 17:48:14 -0500

    moved index.html out of src

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
         outDir: "../dist",
         rollupOptions: {
             input: {
-                main: resolve(__dirname, "src/index.html"),
-                swimmer_profiles: resolve(__dirname, "src/public/swimmer-profiles.html"),
-                register: resolve(__dirname, "src/public/register.html"),
-                state_cuts: resolve(__dirname, "src/public/state-cuts.html")
+                main: resolve(__dirname, "index.html"),
+                swimmer_profiles: resolve(__dirname, "public/swimmer-profiles.html"),
+                register: resolve(__dirname, "public/register.html"),
+                state_cuts: resolve(__dirname, "public/state-cuts.html")
             },
         },
     },
